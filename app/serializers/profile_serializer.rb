17b3fdcb00fedf5593ebadbcafcf307e115a786e
Monica Gerard <mlgvla@gmail.com>
2022-10-05 20:37:53 -0400

    create user, profile, seeds

---
+++ app/serializers/profile_serializer.rb
@@ -0,0 +1,4 @@
+class ProfileSerializer < ActiveModel::Serializer
+  attributes :id, :first_name, :last_name, :email
+  has_one :user
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: "mlgvla", password: "123")
+User.create(username: "tlsbsn", password: "123")
+User.create(username: "nbsgtr", password: "123")
+
+Profile.create(first_name: "Monica", last_name: "Gerard", email: "monica@test.com", user_id: 1)
+Profile.create(first_name: "Tom", last_name: "Sefcovic", email: "tom@test.com", user_id: 2)
+Profile.create(first_name: "Nate", last_name: "Sefcovic", email: "nate@test.com", user_id: 3)
+
+puts "Done Seeding!"
